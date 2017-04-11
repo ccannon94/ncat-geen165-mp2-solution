87e03c69c9b3f19577b4c8a65ce5c18eee1b1146
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-11 16:33:49 -0400

    Create NetBeans project

---
+++ mp2/src/mp2/Mp2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp2;
+
+/**
+ *
+ * @author Chris
+ */
+public class Mp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
