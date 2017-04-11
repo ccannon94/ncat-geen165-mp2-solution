f54afceb779d22fb0b5166307a538696d5711362
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-11 17:25:15 -0400

    Move classes to MP2 package

--- mp2/src/mp2/Mp2.java
+++ mp2/src/mp2/Mp2.java
@@ -16,6 +16,7 @@ public class Mp2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Vehicle myVehicle = new Vehicle()
     }
     
 }
--- mp2/src/mp2/Vehicle.java
+++ mp2/src/mp2/Vehicle.java
@@ -1,3 +1,5 @@
+package mp2;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
