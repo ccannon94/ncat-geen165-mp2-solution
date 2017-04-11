278a8c080c6517c1397c97cdf4ae42b3448a6b0a
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-11 16:49:03 -0400

    Implement fields of Automobile

--- mp2/src/Automobile.java
+++ mp2/src/Automobile.java
@@ -8,6 +8,50 @@
  *
  * @author Chris
  */
-public class Automobile {
-    
+public class Automobile extends Vehicle{
+    private boolean hybrid;
+    private int maxPassengers;
+    private float trunkSpace;
+
+    /**
+     * @return the hybrid
+     */
+    public boolean isHybrid() {
+        return hybrid;
+    }
+
+    /**
+     * @param hybrid the hybrid to set
+     */
+    public void setHybrid(boolean hybrid) {
+        this.hybrid = hybrid;
+    }
+
+    /**
+     * @return the maxPassengers
+     */
+    public int getMaxPassengers() {
+        return maxPassengers;
+    }
+
+    /**
+     * @param maxPassengers the maxPassengers to set
+     */
+    public void setMaxPassengers(int maxPassengers) {
+        this.maxPassengers = maxPassengers;
+    }
+
+    /**
+     * @return the trunkSpace
+     */
+    public float getTrunkSpace() {
+        return trunkSpace;
+    }
+
+    /**
+     * @param trunkSpace the trunkSpace to set
+     */
+    public void setTrunkSpace(float trunkSpace) {
+        this.trunkSpace = trunkSpace;
+    }
 }
