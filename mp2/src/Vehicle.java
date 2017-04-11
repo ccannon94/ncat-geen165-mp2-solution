fe6445d619fcad502ffcfcd567155d941cb4b2c7
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-11 16:41:36 -0400

    Add fields to Vehicle class

--- mp2/src/Vehicle.java
+++ mp2/src/Vehicle.java
@@ -9,5 +9,64 @@
  * @author Chris
  */
 public class Vehicle {
-    
+    protected String make;
+    protected String model;
+    protected String vin;
+    protected int year;
+
+    /**
+     * @return the make
+     */
+    public String getMake() {
+        return make;
+    }
+
+    /**
+     * @param make the make to set
+     */
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the vin
+     */
+    public String getVin() {
+        return vin;
+    }
+
+    /**
+     * @param vin the vin to set
+     */
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    /**
+     * @return the year
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
