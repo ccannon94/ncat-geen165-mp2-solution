609e2336d026ae4ea0244d1448c85eef6845626a
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-11 18:21:49 -0400

    Add fields to Fleet class

--- mp2/src/mp2/Fleet.java
+++ mp2/src/mp2/Fleet.java
@@ -5,10 +5,47 @@
  */
 package mp2;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Chris
  */
 public class Fleet {
+    private String fleetName;
+    ArrayList<Vehicle> fleetList;
+
+    /**
+     * @return the fleetName
+     */
+    public String getFleetName() {
+        return fleetName;
+    }
+
+    /**
+     * @param fleetName the fleetName to set
+     */
+    public void setFleetName(String fleetName) {
+        this.fleetName = fleetName;
+    }
     
+    public int getNumVehicles(){
+        return fleetList.size();
+    }
+    
+    public Vehicle getVehcile(int index){
+        return fleetList.get(index);
+    }
+    
+    public void addVehicle(Vehicle item){
+        fleetList.add(item);
+    }
+    
+    public void setVehicle(int index, Vehicle item){
+        fleetList.set(index, item);
+    }
+    
+    public Vehicle removeVehicle(int index){
+        return fleetList.remove(index);
+    }
 }
