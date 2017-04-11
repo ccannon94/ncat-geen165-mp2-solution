6d892c5a1fc8cea0ca2c70099da1f7a7fd729782
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-11 17:06:01 -0400

    Add fields to PassengerVan

--- mp2/src/PassengerVan.java
+++ mp2/src/PassengerVan.java
@@ -8,6 +8,50 @@
  *
  * @author Chris
  */
-public class PassengerVan {
-    
+public class PassengerVan extends Van{
+    private int numSeatRows;
+    private int maxPassengers;
+    private boolean dvdPlayer;
+
+    /**
+     * @return the numSeatRows
+     */
+    public int getNumSeatRows() {
+        return numSeatRows;
+    }
+
+    /**
+     * @param numSeatRows the numSeatRows to set
+     */
+    public void setNumSeatRows(int numSeatRows) {
+        this.numSeatRows = numSeatRows;
+    }
+
+    /**
+     * @return the maxPassengers
+     */
+    public int getMaxPassengers() {
+        return maxPassengers;
+    }
+
+    /**
+     * @param maxPassengers the maxPassengers to set
+     */
+    public void setMaxPassengers(int maxPassengers) {
+        this.maxPassengers = maxPassengers;
+    }
+
+    /**
+     * @return the dvdPlayer
+     */
+    public boolean isDvdPlayer() {
+        return dvdPlayer;
+    }
+
+    /**
+     * @param dvdPlayer the dvdPlayer to set
+     */
+    public void setDvdPlayer(boolean dvdPlayer) {
+        this.dvdPlayer = dvdPlayer;
+    }
 }
