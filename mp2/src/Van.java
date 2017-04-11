ae8aecf74dc9136c8ae25eb197a0cfb0c3375375
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-11 16:58:52 -0400

    Add fields to Van

--- mp2/src/Van.java
+++ mp2/src/Van.java
@@ -8,6 +8,35 @@
  *
  * @author Chris
  */
-public class Van {
-    
+public class Van extends Vehicle{
+    protected float clearanceHeight;
+    protected int numWindows;
+
+    /**
+     * @return the clearanceHeight
+     */
+    public float getClearanceHeight() {
+        return clearanceHeight;
+    }
+
+    /**
+     * @param clearanceHeight the clearanceHeight to set
+     */
+    public void setClearanceHeight(float clearanceHeight) {
+        this.clearanceHeight = clearanceHeight;
+    }
+
+    /**
+     * @return the numWindows
+     */
+    public int getNumWindows() {
+        return numWindows;
+    }
+
+    /**
+     * @param numWindows the numWindows to set
+     */
+    public void setNumWindows(int numWindows) {
+        this.numWindows = numWindows;
+    }
 }
