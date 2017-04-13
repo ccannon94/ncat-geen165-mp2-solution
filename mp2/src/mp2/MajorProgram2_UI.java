f1d240bc17c65849111281caf784a53e93425177
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-13 16:43:57 -0400

    Revert "Add load button to handle()"
    This reverts commit b9b6363e05e57c9a4d99fa7fa7f17da4465ab772.

--- mp2/src/mp2/MajorProgram2_UI.java
+++ mp2/src/mp2/MajorProgram2_UI.java
@@ -5,7 +5,6 @@
  */
 package mp2;
 
-import java.io.File;
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -14,7 +13,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -22,15 +20,13 @@ import javafx.stage.Stage;
  * @author Chris
  */
 public class MajorProgram2_UI extends Application implements EventHandler{
-    Fleet activeFleet;
-    
     Scene mainMenu;
-    Label fleetLabel;
+    Fleet activeFleet;
     
     @Override
     public void start(Stage primaryStage) throws Exception {
         BorderPane pane = new BorderPane();
-        fleetLabel = new Label("Please Load a Fleet");
+        Label fleetLabel = new Label("Please Load a Fleet");
         
         pane.setTop(fleetLabel);
         pane.setCenter(createControlBox());
@@ -61,18 +57,7 @@ public class MajorProgram2_UI extends Application implements EventHandler{
     
     @Override
     public void handle(Event event) {
-        Button sourceButton = (Button) event.getSource();
-        
-        if(sourceButton != null && sourceButton.getText().equals("Load")){
-            FileChooser loadChooser = new FileChooser();
-            loadChooser.setTitle("Select an input Fleet file");
-            loadChooser.setInitialDirectory(new File(System.getProperty("user.home")));
-            loadChooser.getExtensionFilters().setAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-            File inputFile = loadChooser.showOpenDialog(null);
-            
-            activeFleet.loadFleet(inputFile.getAbsolutePath());
-            fleetLabel.setText(activeFleet.getFleetName());
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
