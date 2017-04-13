8bc2841d46e4220a6cf3b6585b0d5461a214e4b3
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-13 15:01:06 -0400

    Populate UI with control buttons

--- mp2/src/mp2/MajorProgram2_UI.java
+++ mp2/src/mp2/MajorProgram2_UI.java
@@ -8,6 +8,10 @@ package mp2;
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -18,11 +22,28 @@ public class MajorProgram2_UI extends Application implements EventHandler{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        BorderPane pane = new BorderPane();
+        Label fleetLabel = new Label();
+        
+        pane.setCenter(createControlBox());
     }
 
     public void launchGUI(String[] args){
         launch(args);
+    }
+    
+    private HBox createControlBox(){
+        HBox controlBox = new HBox();
+        
+        Button viewButton = new Button("View");
+        Button addButton = new Button("Add");
+        Button loadButton = new Button("Load");
+        Button saveButton = new Button("Save");
+        Button exitButton = new Button("Exit");
+        
+        controlBox.getChildren().addAll(viewButton, addButton, loadButton, saveButton, exitButton);
+        
+        return controlBox;
     }
     
     @Override
